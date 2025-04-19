e550afd17afa86172d8632471e30733e8d104cd4
xingchenwl <sd_bz_zh@163.com>
2025-04-20 03:03:09 +0800

    Update FloatingPlugin.kt

--- android/src/main/kotlin/eu/wroblewscy/marcin/floating/floating/FloatingPlugin.kt
+++ android/src/main/kotlin/eu/wroblewscy/marcin/floating/floating/FloatingPlugin.kt
@@ -21,93 +21,95 @@ import kotlin.concurrent.fixedRateTimer
 
 /** FloatingPlugin */
 class FloatingPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
-    private lateinit var channel: MethodChannel
-    private lateinit var context: Context
-    private lateinit var activity: Activity
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel: MethodChannel
+  private lateinit var context: Context
+  private lateinit var activity: Activity
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "floating")
-        channel.setMethodCallHandler(this)
-        context = flutterPluginBinding.applicationContext
-    }
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "floating")
+    channel.setMethodCallHandler(this)
+    context = flutterPluginBinding.applicationContext
+  }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        if (call.method == "enablePip") {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val builder = PictureInPictureParams.Builder()
-                    .setAspectRatio(
-                        Rational(
-                            call.argument("numerator") ?: 16,
-                            call.argument("denominator") ?: 9
-                        )
-                    )
-                val sourceRectHintLTRB = call.argument<List<Int>>("sourceRectHintLTRB")
-                if (sourceRectHintLTRB?.size == 4) {
-                    val bounds = Rect(
-                        sourceRectHintLTRB[0],
-                        sourceRectHintLTRB[1],
-                        sourceRectHintLTRB[2],
-                        sourceRectHintLTRB[3]
-                    )
-                    builder.setSourceRectHint(bounds)
-                }
-
-                val autoEnable = call.argument<Boolean>("autoEnable") ?: false
-                if (autoEnable && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    builder.setAutoEnterEnabled(true)
-                    activity.setPictureInPictureParams(builder.build())
-                    result.success(true)
-                    return
-                } else if (autoEnable && Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
-                    result.error(
-                        "AutoEnable not available",
-                        "AutoEnable is only available on SDK higher than 31",
-                        "Current SDK: ${Build.VERSION.SDK_INT}, required: >=31"
-                    )
-                    return
-                }
-
-                result.success(
-                    activity.enterPictureInPictureMode(builder.build())
-                )
-            } else {
-                result.success(activity.enterPictureInPictureMode())
-            }
-        } else if (call.method == "pipAvailable") {
-            result.success(
-                activity.packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)
+  @RequiresApi(Build.VERSION_CODES.N)
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method == "enablePip") {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val builder = PictureInPictureParams.Builder()
+          .setAspectRatio(
+            Rational(
+              call.argument("numerator") ?: 16,
+              call.argument("denominator") ?: 9
             )
-        } else if (call.method == "inPipAlready") {
-            result.success(
-                activity.isInPictureInPictureMode
-            )
-        } else {
-            result.notImplemented()
+          )
+        val sourceRectHintLTRB = call.argument<List<Int>>("sourceRectHintLTRB")
+        if (sourceRectHintLTRB?.size == 4) {
+          val bounds = Rect(
+            sourceRectHintLTRB[0],
+            sourceRectHintLTRB[1],
+            sourceRectHintLTRB[2],
+            sourceRectHintLTRB[3]
+          )
+          builder.setSourceRectHint(bounds)
         }
+
+
+        val autoEnable = call.argument<Boolean>("autoEnable") ?: false
+        if (autoEnable && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+          builder.setAutoEnterEnabled(true)
+          activity.setPictureInPictureParams(builder.build())
+          result.success(true)
+          return
+        } else if (autoEnable && Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+          result.error(
+            "AutoEnable not available",
+            "AutoEnable is only available on SDK higher than 31",
+            "Current SDK: ${Build.VERSION.SDK_INT}, required: >=31"
+          )
+          return
+        }
+
+        result.success(
+            activity.enterPictureInPictureMode(builder.build())
+        )
+      } else {
+        result.success(activity.enterPictureInPictureMode())
+      }
+    } else if (call.method == "pipAvailable") {
+      result.success(
+          activity.packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)
+      )
+    } else if (call.method == "inPipAlready") {
+      result.success(
+          activity.isInPictureInPictureMode
+      )
+    } else
+     {
+      result.notImplemented()
     }
+  }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
-    }
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
 
-    override fun onDetachedFromActivity() {}
+  override fun onDetachedFromActivity() {}
 
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-        useBinding(binding)
-    }
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    useBinding(binding)
+  }
 
-    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        useBinding(binding)
-    }
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    useBinding(binding)
+  }
 
-    override fun onDetachedFromActivityForConfigChanges() {}
+  override fun onDetachedFromActivityForConfigChanges() {}
 
-    fun useBinding(binding: ActivityPluginBinding) {
-        activity = binding.activity
-    }
+  fun useBinding(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
 }
